62a21fa0d6e2bc1032cc65ba6e0e8276f7a0deb4
renxia <lzwy0820@qq.com>
2023-10-26 16:52:23 +0800

    feat(files-cate): 增加按文件类型将指定的文件移动到对应的目录功能

--- examples/files-cate/index.ts
+++ examples/files-cate/index.ts
@@ -1,9 +1,11 @@
-import { copyFileSync, existsSync, promises, readdirSync, rmSync, rmdirSync, statSync } from 'node:fs';
+import { copyFileSync, existsSync, mkdir, promises, readdirSync, rmSync, rmdirSync, statSync } from 'node:fs';
 import { basename, dirname, resolve } from 'node:path';
 import { exec } from 'node:child_process';
-import { concurrency, dateFormat, mkdirp } from '@lzwme/fe-utils';
+import { color, concurrency, dateFormat, getLogger, mkdirp } from '@lzwme/fe-utils';
 import glob from 'fast-glob';
 import minimist from 'minimist';
+
+const logger = getLogger('[FILE-CATE]');
 
 interface Options {
   src?: string;
@@ -14,19 +16,41 @@ interface Options {
   cateMin?: number;
   dryRun?: boolean;
 }
+
+async function cpOrMvFile(idx: number, filepath: string, destpath: string, mode: Options['mode'] = 'copy', overwide = false) {
+  if (existsSync(destpath) && !overwide) return false;
+
+  logger.info(`[${color.magenta(idx)}][${mode}]: "${color.gray(filepath)}" => "${color.green(destpath)}"`);
+  mkdirp(dirname(destpath));
+
+  if (mode === 'mv-shell') {
+    await new Promise(resolve => {
+      exec(`mv "${filepath}" "${destpath}"`, { shell: process.platform === 'win32' ? 'powershell' : '/bin/sh' }, () => resolve(true));
+    });
+  } else if (mode === 'copy') {
+    await promises.copyFile(filepath, destpath);
+  } else if (mode === 'mv') {
+    await promises.copyFile(filepath, destpath);
+    await promises.rm(filepath);
+  }
+
+  return true;
+}
+
 /** 将指定目录的文件，按修改日期为目录分类整理 */
 async function classifyFilesByDate(options: Options) {
   options = { src: process.cwd(), destDir: 'dest', mode: 'copy', cateMin: 10, ...options };
 
   let srcList = await glob('**/*', { absolute: true, cwd: options.src, onlyFiles: true });
-  const destList = await glob('**/*', { absolute: true, cwd: options.destDir, onlyFiles: true });
-  const destFileSet = new Set((destList.map(d => basename(d))));
   const statInfo = {
     total: srcList.length,
     count: 0,
     date: {} as Record<string, { total: number; list: { src: string; dest: string }[] }>,
   };
 
+  // 过滤已存在于目标目录中的同名文件
+  const destList = await glob('**/*', { absolute: true, cwd: options.destDir, onlyFiles: true });
+  const destFileSet = new Set((destList.map(d => basename(d))));
   srcList = srcList.filter(srcFile => !destFileSet.has(basename(srcFile)));
 
   for (const filepath of srcList) {
@@ -46,15 +70,16 @@ async function classifyFilesByDate(options: Options) {
   for (const [date, info] of Object.entries(statInfo.date)) {
     if (info.total < options.cateMin!) {
       const destDateDir = resolve(options.destDir!, date.slice(0, 4), date);
-      if (existsSync(destDateDir))  {
-        if (options.mode === 'copy') {
-          console.log('rmdir', destDateDir, readdirSync(destDateDir, 'utf8').length, info.total);
-          rmdirSync(destDateDir, { recursive: true });
-        } else continue;
-      }
+      if (existsSync(destDateDir) && options.mode !== 'copy') continue;
+      // if (existsSync(destDateDir)) {
+      //   if (options.mode === 'copy') {
+      //     logger.info('rmdir', destDateDir, readdirSync(destDateDir, 'utf8').length, info.total);
+      //     rmdirSync(destDateDir, { recursive: true });
+      //   } else continue;
+      // }
 
       const nDate = date.slice(0, -3);
-      info.list.forEach(item => item.dest = resolve(options.destDir!, date.slice(0, 4), nDate, basename(item.dest)));
+      info.list.forEach(item => (item.dest = resolve(options.destDir!, date.slice(0, 4), nDate, basename(item.dest))));
       if (!statInfo.date[nDate]) statInfo.date[nDate] = info;
       else {
         statInfo.date[nDate].total += info.total;
@@ -63,7 +88,7 @@ async function classifyFilesByDate(options: Options) {
       delete statInfo.date[date];
     }
   }
-  console.log(statInfo, `文件总数：`, statInfo.total, `目录数：`, Object.keys(statInfo.date).length, options);
+  logger.info(statInfo, `文件总数：`, statInfo.total, `目录数：`, Object.keys(statInfo.date).length, options);
 
   // 开始复制或移动文件
   const fns = Object.values(statInfo.date).map(info => {
@@ -73,19 +98,7 @@ async function classifyFilesByDate(options: Options) {
         if (dest === filepath) continue;
 
         if (!existsSync(dest) || options.overwide) {
-          mkdirp(dirname(dest));
-          console.log(`[${statInfo.count++}] ${options.mode}: "${filepath}" "${dest}"`);
-
-          if (options.mode === 'mv-shell') {
-            await new Promise(resolve => {
-              exec(`mv "${filepath}" "${dest}"`, { shell: process.platform === 'win32' ? 'powershell' : '/bin/sh' }, () => resolve(true));
-            });
-          } else if (options.mode === 'copy') {
-            await promises.copyFile(filepath, dest);
-          } else if (options.mode === 'mv') {
-            await promises.copyFile(filepath, dest);
-            await promises.rm(filepath);
-          }
+          await cpOrMvFile(statInfo.count++, filepath, dest, options.mode, options.overwide);
         }
       }
     };
@@ -96,5 +109,47 @@ async function classifyFilesByDate(options: Options) {
   return statInfo;
 }
 
+/** 将指定目录的文件，按指定的文件类型过滤并移动至指定的目录中 */
+async function filterFileByext(options: Pick<Options, 'src' | 'destDir' | 'mode' | 'overwide'> & { ext?: string[] }) {
+  options = { src: process.cwd(), destDir: 'dest', mode: 'copy', ...options };
+  if (typeof options.ext === 'string') options.ext = [options.ext];
+  if (!Array.isArray(options.ext) || options.ext.length === 0) return logger.error('请指定要归类的文件类型');
+  const exts = options.ext
+    .filter(Boolean)
+    .map(d => d.replace(/^\./, ''))
+    .join(',');
+  const srcList = await glob(`**/*.${exts}`, { absolute: true, cwd: options.src, onlyFiles: true });
+  let count = 0;
+
+  console.log('文件数：', srcList.length, exts);
+
+  if (srcList.length) {
+    const fns = srcList.map(filepath => {
+      return async () => {
+        const destpath = resolve(options.destDir!, basename(filepath));
+        await cpOrMvFile(count++, filepath, destpath, options.mode);
+      };
+    });
+
+    await concurrency(fns);
+    logger.info(`执行完成！文件总数： ${color.cyan(srcList.length)}， 处理文件数： ${count}`);
+  }
+
+  return count;
+}
+
 const argv = minimist(process.argv.slice(2));
-classifyFilesByDate({ src: argv.src, destDir: argv.dest, mode: argv.mode || 'copy', dryRun: argv.dryRun, overwide: false, cateMin: +argv.cateMin || 10 });
+if (argv.type === 'date') {
+  classifyFilesByDate({
+    src: argv.src,
+    destDir: argv.dest,
+    mode: argv.mode || 'copy',
+    dryRun: argv.dryRun,
+    overwide: false,
+    cateMin: +argv.cateMin || 10,
+  });
+} else if (argv.type === 'ext') {
+  filterFileByext({ src: argv.src, destDir: argv.dest, mode: argv.mode || 'copy', overwide: false, ext: argv.ext });
+} else {
+  logger.error('请指定 type 参数');
+}
